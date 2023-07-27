1afc5607063c2f71d0f1ab145c7912ea2198ddd4
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-27 15:41:12 -0300

    set theme provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,20 @@
-import { ThemeProvider } from "styled-components";
-import GlobalStyle from "../styles/resetStyle";
-import { Hydrate } from "next/app";
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../styles/resetStyle';
 
-export const theme2 = {}
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+};
 
-export default function MyApp({ Component, pageProps }) {
- 
+export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme2}>
-        <Hydrate state={pageProps.dehydratedState}>
-          <Component {...pageProps} />
-        </Hydrate>
+    <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
       </ThemeProvider>
     </>
   );
-
 }
