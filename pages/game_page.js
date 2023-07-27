d0ae797933de29e97d69ca4aad0a853ce2b4fa83
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-27 17:32:25 -0300

    update import component

--- pages/game_page.js
+++ pages/game_page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { styled } from 'styled-components'
-import OpenChest from "../components/OpenChestContainer";
+import OpenChest from "../components/OpenChest";
 
 const Container = styled.div`
     display: flex;
