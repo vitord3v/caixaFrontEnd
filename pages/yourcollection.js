9f2a9e64186258d1a8ddbac416a653a34cc13ee9
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-28 13:53:34 -0300

    [feat]update css

--- pages/yourcollection.js
+++ pages/yourcollection.js
@@ -3,14 +3,21 @@ import styled from "styled-components";
 
 const Container = styled.div`
     display: flex;
+    gap: 2rem;
+    margin-left: 300px;
+
+    h1 {
+        color: white;
+    }
 
 `
 
 function yourCollection () {
     return ( 
         <>
+        <Container>
             <h1> Sua Coleção </h1>
-            <Container>
+            
                 <SoccerCard />
                 <SoccerCard />
                 <SoccerCard />
