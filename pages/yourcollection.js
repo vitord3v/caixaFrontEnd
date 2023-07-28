75fe2a228372300a17515ee81a4b534dc226ed64
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-28 15:06:04 -0300

    ajust css styles and connectWallet feature

--- pages/yourcollection.js
+++ pages/yourcollection.js
@@ -1,3 +1,5 @@
+import { useCallback } from 'react';
+import Web3 from 'web3';
 import SoccerCard from "../components/SoccerCard";
 import styled from "styled-components";
 
@@ -6,25 +8,52 @@ const Container = styled.div`
     gap: 2rem;
     margin-left: 300px;
 
-    h1 {
-        color: white;
+    .walletButton {
+        background-color: #F09000;
     }
-
 `
+function YourCollection () {
+    const connectWallet = useCallback(async () => {
+        if (window.ethereum) {
+            window.web3 = new Web3(window.ethereum);
+            await window.ethereum.enable();
 
-function yourCollection () {
+            const polygonNetwork = {
+                chainId: '0x89',
+                chainName: 'Matic Network',
+                nativeCurrency: {
+                    name: 'MATIC',
+                    symbol: 'MATIC',
+                    decimals: 18
+                },
+                rpcUrls: ['https://rpc-mainnet.maticvigil.com/'],
+                blockExplorerUrls: ['https://explorer.matic.network/']
+            };
+
+            await window.ethereum.request({
+                method: 'wallet_addEthereumChain',
+                params: [polygonNetwork]
+            });
+        } else {
+            alert('MetaMask não encontrada. Você precisa instalar o MetaMask para usar este aplicativo.');
+        }
+    }, []);
+
     return ( 
         <>
+        
         <Container>
+            <div>
+                <button className='walletButton' onClick={connectWallet}>Conectar Carteira</button>
+            </div>
             <h1> Sua Coleção </h1>
-            
-                <SoccerCard />
-                <SoccerCard />
-                <SoccerCard />
-            </Container>
+            <SoccerCard />
+            <SoccerCard />
+            <SoccerCard />
+        </Container>
                 
         </>
     )
 }
 
-export default yourCollection;
+export default YourCollection;
