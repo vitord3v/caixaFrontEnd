6fb71b641fae641b0bd29811df9d0d198b646d66
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-28 10:10:34 -0300

    [fix] change game_page to index

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,74 @@
-import styled from 'styled-components';
+import Image from "next/image";
+import { styled } from 'styled-components'
+import OpenChest from "../components/OpenChest";
+import BetGame from '../components/BetGame'
+import Bingo from "../components/Bingo";
+
+const Container = styled.div`
+    display: flex;
+    justify-content: space-between;
+    max-width: 1000px;
+    margin: 0 auto;
+    color: white;
+
+`
+
+const BetContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    h1 {
+        margin-bottom: 2rem;
+        font-size: 32px;
+        color:white;
+    }
+
+    p {
+        font-size: 64px;
+    }
+
+    
+`
+
+const ChestContainer = styled.div`
+    padding: 1rem;
+    border:1px solid white;
+    border-radius: 40px;
+    max-width:650px;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    p {
+        
+    }
+`
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Hello Next.js</h1>
-      <Button>Click Me!</Button>
-    </div>
-  )
+    return(
+        <>
+        <Container>
+            <BetContainer>
+                <div>
+                    <h1>Suas Apostas</h1>
+                    <p>
+                        0/10
+                    </p>
+                </div>
+            </BetContainer>
+            
+            <ChestContainer>
+                <p>
+                    Faça 10 apostas no timemania e abra um baú com NFT’s exclusivos!
+                </p>
+                <Image src='/chest.png' width='169' height='124' alt="Baú de ouro para abrir sua caixa" />
+            </ChestContainer>
+            
+        </Container>
+        <OpenChest />
+        <BetGame />
+        <Bingo/>
+        </>
+    );
 }
 
-const Button = styled.button`
-  background-color: #007bff;
-  color: #fff;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-`;
