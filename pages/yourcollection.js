9696281e0bd1e994b89cc028949ff7bb4ca4a4d0
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-29 00:50:13 -0300

    [feat] getNFts

--- pages/yourcollection.js
+++ pages/yourcollection.js
@@ -1,8 +1,10 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import Web3 from 'web3';
 import SoccerCard from "../components/SoccerCard";
 import styled from "styled-components";
 import { bingoColor } from '../colors/colors';
+
+import { Alchemy, Network } from "alchemy-sdk";
 
 const Container = styled.div`
     display: flex;
@@ -46,10 +48,40 @@ const Container = styled.div`
     }
 `
 function YourCollection () {
+
+    const [userAddress, setUserAddress] = useState(null)
+
+    const getNftByAdress = async () => {
+
+        const nftManiaContract = "0x1882b841564a11675729abff14c2b6ecbb5dfb14"
+
+        const config = {
+            apiKey:'-uTYzNDjfm7ac3cAPERTDB4ZE995HPqH',
+            network: Network.MATIC_MAINNET,
+        };
+        const alchemy = new Alchemy(config)
+
+        if(userAddress) {
+            const nfts = await alchemy.nft.getNftsForOwner(userAddress)
+            const nftList = nfts["ownedNfts"]
+
+            const ownedNfts = nftList.filter((nft) => nft.contract.address === nftManiaContract)
+            console.log(ownedNfts)
+        }
+    }
+
+    useEffect(() => {getNftByAdress()},[userAddress]) 
+
     const connectWallet = useCallback(async () => {
         if (window.ethereum) {
             window.web3 = new Web3(window.ethereum);
             await window.ethereum.enable();
+
+            const accounts = await window.web3.eth.getAccounts();
+            if (accounts.length > 0) {
+            setUserAddress(accounts[0])
+            console.log("Endereço da carteira conectada:", accounts[0]);
+            }
 
             const polygonNetwork = {
                 chainId: '0x89',
