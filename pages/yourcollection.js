ef4c2302472e8ee2d6a7537edd686f64d897ad88
Nathália Brum <natybrum.97@gmail.com>
2023-07-29 05:44:32 -0300

    Versão com My collection

--- pages/yourcollection.js
+++ pages/yourcollection.js
@@ -12,14 +12,15 @@ const Container = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    margin-left: 200px;
-    margin-top: 100px;
+    margin-left: 100px;
+    margin-bottom:75px;
+    margin-top:75px;
 
     .head{
         display: flex;
-        gap: 10px;
         align-items: center;
-        justify-content: center;
+        justify-content: space-around;
+        width:894px;
     }
 
     h1{
@@ -30,11 +31,16 @@ const Container = styled.div`
 
     .walletButton {
         background-color: #F09000;
+        width:250px;
     }
 
     .cards{
         display: flex;
+        flex-wrap:wrap;
         gap: 10px;
+        width:894px;
+        height:auto;
+
     }
 
     button{
@@ -47,30 +53,33 @@ const Container = styled.div`
         }
     }
 `
-function YourCollection () {
+function YourCollection() {
 
-    const [userAddress, setUserAddress] = useState(null)
+    const [userAddress, setUserAddress] = useState(null);
+    const [nftsEspecificacoes, setNftsEspecificacoes] = useState([]);
 
     const getNftByAdress = async () => {
 
         const nftManiaContract = "0x1882b841564a11675729abff14c2b6ecbb5dfb14"
 
         const config = {
-            apiKey:'-uTYzNDjfm7ac3cAPERTDB4ZE995HPqH',
+            apiKey: '-uTYzNDjfm7ac3cAPERTDB4ZE995HPqH',
             network: Network.MATIC_MAINNET,
         };
         const alchemy = new Alchemy(config)
 
-        if(userAddress) {
+        if (userAddress) {
             const nfts = await alchemy.nft.getNftsForOwner(userAddress)
+            console.log(nfts)
             const nftList = nfts["ownedNfts"]
 
             const ownedNfts = nftList.filter((nft) => nft.contract.address === nftManiaContract)
+            setNftsEspecificacoes(ownedNfts);
             console.log(ownedNfts)
         }
     }
 
-    useEffect(() => {getNftByAdress()},[userAddress]) 
+    useEffect(() => { getNftByAdress() }, [userAddress])
 
     const connectWallet = useCallback(async () => {
         if (window.ethereum) {
@@ -79,8 +88,8 @@ function YourCollection () {
 
             const accounts = await window.web3.eth.getAccounts();
             if (accounts.length > 0) {
-            setUserAddress(accounts[0])
-            console.log("Endereço da carteira conectada:", accounts[0]);
+                setUserAddress(accounts[0])
+                console.log("Endereço da carteira conectada:", accounts[0]);
             }
 
             const polygonNetwork = {
@@ -104,22 +113,39 @@ function YourCollection () {
         }
     }, []);
 
-    return ( 
+    return (
         <>
-        
         <Container>
-            <div className='head'>
-            <h1> Sua Coleção </h1>
-                <button className='walletButton' onClick={connectWallet}>Conectar Carteira</button>
-            </div>
-           
-            <div className='cards'>
-            <SoccerCard turned={false} name='Silêncio no maraca' source='/soccer-player.svg' alt_text=''/>
-            <SoccerCard turned={false} name='Dinamitando' source='/dinamite.png' alt_text=''/>
-            <SoccerCard turned={false} name='Silêncio no maraca' source='/soccer-player.svg' alt_text=''/>
-            </div>
-        </Container>
-                
+                <div className='head'>
+                    <h1> Minha Coleção </h1>
+                </div>
+
+                <div className='cards'>
+                    {nftsEspecificacoes.map((nfts) => (
+
+                        <SoccerCard key={nfts.tokenId} quantidade={nfts.balance} turned={false} name={nfts.title} source={nfts.media[0].gateway} alt_text={nfts.rawMetadata.description} />
+
+                    ))}
+
+                </div>
+            </Container>
+
+            <Container>
+                <div className='head'>
+                    <h1> Coleção na sua Carteira </h1>
+                    <button className='walletButton' onClick={connectWallet}>Conectar Carteira</button>
+                </div>
+
+                <div className='cards'>
+                    {nftsEspecificacoes.map((nfts) => (
+
+                        <SoccerCard key={nfts.tokenId} quantidade={nfts.balance} turned={false} name={nfts.title} source={nfts.media[0].gateway} alt_text={nfts.rawMetadata.description} />
+
+                    ))}
+
+                </div>
+            </Container>
+
         </>
     )
 }
